06874e2fd307cada4a3a0604e3f0a6a2a8bfbb2d
Marcos Oliveira <48111913+mulequim@users.noreply.github.com>
2025-10-16 18:11:49 -0300

    Update lotofacil.py

--- lotofacil.py
+++ lotofacil.py
@@ -39,7 +39,7 @@ from github import Github  # usado apenas na função atualizar_csv_github (se n
 # ---------------------------
 # Carregar dados do CSV
 # ---------------------------
-def carregar_dados(file_path="Lotofacil.csv"):
+def carregar_dados(file_path="Lotofacil_Concursos.csv"):
     """
     Lê o arquivo CSV da Lotofácil, mesmo com campos mistos (',' e ';').
     Faz detecção automática e normaliza os dados.
